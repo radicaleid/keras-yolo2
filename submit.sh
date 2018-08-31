f383ece134bed2d7e3b91c1c887a19d8c526274c
Taylor Childers <taylor@jtchilders.com>
2018-08-31 13:46:49 +0000

    updates for horovod

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#COBALT -n 1
+#COBALT -n 2
 #COBALT -t 60
 #COBALT -q debug-flat-quad
 #COBALT -A datascience
@@ -8,7 +8,8 @@
 MODELDIR=/projects/datascience/parton/atlasml/keras-yolo2
 
 module load tensorflow
-echo PYTHON_VERSION=$(python --version)
+module load horovod
+echo PYTHON_VERSION=$(python --version 2>&1 )
 
 export OMP_NUM_THREADS=128
 export KMP_BLOCKTIME=1
@@ -17,5 +18,5 @@ export KMP_SETTINGS=1
 
 aprun -n 1 -N 1 python -c "import tensorflow as tf;print('tensorflow version: ',tf.__version__)"
 
-aprun -n 1 -N 1 -d 128 -j 2 -cc depth python $MODELDIR/train.py -c config.json
+aprun -n 2 -N 1 -d 128 -j 2 -cc depth python $MODELDIR/train.py -c config.json
 
