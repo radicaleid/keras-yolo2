36d0e381cff18625072f895756f5425b191797ab
Taylor Childers <taylor@jtchilders.com>
2018-09-19 14:27:41 +0000

    updated submit script

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#COBALT -n 2
+#COBALT -n 1
 #COBALT -t 60
 #COBALT -q debug-flat-quad
 #COBALT -A datascience
@@ -7,16 +7,23 @@
 
 MODELDIR=/projects/datascience/parton/atlasml/keras-yolo2
 
-module load tensorflow
+module unload darshan
 module load horovod
+module load keras
 echo PYTHON_VERSION=$(python --version 2>&1 )
 
-export OMP_NUM_THREADS=128
-export KMP_BLOCKTIME=1
-export KMP_SETTINGS=1
-# export KMP_AFFINITY="granularity=fine,compact,1,0"
+source /opt/intel/vtune_amplifier/amplxe-vars.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/vtune_amplifier/lib64
+export PE_RANK=$ALPS_APP_PE
+export PMI_NO_FORK=1
 
-aprun -n 1 -N 1 python -c "import tensorflow as tf;print('tensorflow version: ',tf.__version__)"
-
-aprun -n 2 -N 1 -d 128 -j 2 -cc depth python $MODELDIR/train.py -c config.json
-
+env | sort
+PPN=2
+NTHDS=64
+INTER=1
+COMBO=${PPN}_${NTHDS}_${INTER}
+echo [$SECONDS] run job PPN=$PPN NTHDS=$NTHDS INTER=$INTER COBALT_PARTSIZE=$COBALT_PARTSIZE
+aprun -n $(( ${COBALT_PARTSIZE} * ${PPN})) -N ${PPN} -cc depth -d ${NTHDS} -j 2 \
+   amplxe-cl -collect advanced-hotspots -finalization-mode=none -r ./${COBALT_JOBID}_amplxe -data-limit=0  -- \
+   python $MODELDIR/train.py -c config.json --num_intra=$NTHDS --num_inter=$INTER --tb_logdir=logs/${COBALT_JOBID}_${COMBO}_logs --horovod
+echo [$SECONDS] exited $?
