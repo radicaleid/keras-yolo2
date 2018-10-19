fixes for accuracy calculation

frontend.py
@@ -312,6 +312,10 @@ class YOLO(object):
             loss = tf.Print(loss, [true_box_wh[indices[0][0],indices[0][1],indices[0][2],indices[0][3]]], message='true_box_wh \t', summarize=1000)
             loss = tf.Print(loss, [pred_box_wh[indices[0][0],indices[0][1],indices[0][2],indices[0][3]]], message='pred_box_wh \t', summarize=1000)
 
+
+            loss = tf.Print(loss, [true_box_xy[indices[0][0],indices[0][1],indices[0][2],indices[0][3]]], message='true_box_xy \t', summarize=1000)
+            loss = tf.Print(loss, [pred_box_xy[indices[0][0],indices[0][1],indices[0][2],indices[0][3]]], message='pred_box_xy \t', summarize=1000)
+
             loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
             loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
             loss = tf.Print(loss, [loss_conf], message='Loss Conf \t', summarize=1000)
@@ -445,7 +449,7 @@ class YOLO(object):
             callbacks.append(self.hvd.callbacks.MetricAverageCallback())
 
             # create tensorboard callback
-            tensorboard = TB2(evaluate=self.evaluate,generator=valid_generator,log_every=10,log_dir=log_path,update_freq='batch')
+            tensorboard = TB2(evaluate=self.evaluate,generator=valid_generator,log_every=2,log_dir=log_path,update_freq='batch')
             callbacks.append(tensorboard)
             if self.hvd.rank() == 0:
                verbose = self.config_file['train']['verbose']
@@ -500,11 +504,7 @@ class YOLO(object):
                                         save_best_only=True,
                                         mode='min',
                                         period=1)
-           tensorboard = TensorBoard(log_dir='./logs',
-                                     histogram_freq=0,
-                                     # write_batch_performance=True,
-                                     write_graph=True,
-                                     write_images=False)
+           tensorboard = TB2(evaluate=self.evaluate,generator=valid_generator,log_every=10,log_dir=log_path,update_freq='batch')
 
            callbacks = [
                early_stop,
@@ -669,7 +669,7 @@ class YOLO(object):
         input_image = np.expand_dims(input_image, 0)
         dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
 
-        netout = self.model.predict([image, dummy_array])[0]
+        netout = self.model.predict([input_image, dummy_array])[0]
         boxes  = decode_netout(netout, self.anchors, self.nb_class)
 
         return boxes

preprocessing.py
@@ -150,20 +150,30 @@ class BatchGenerator(Sequence):
         return np.stack(dense_mats)  # tf.sparse_to_dense(new_indices, shape, np.concatenate(new_data)).eval()
 
     def load_annotation(self, i):
-        file_index = int(i / self.evts_per_file)
-        image_index = i % self.evts_per_file
-        file_content = np.load(self.filelist[file_index])
+        if self.sparse:
+            file_content = np.load(self.filelist[i])
+            objs = file_content[2]
+        else:
+            file_index = int(i / self.evts_per_file)
+            image_index = i % self.evts_per_file
+            file_content = np.load(self.filelist[file_index])
+            objs = file_content['truth'][image_index]
+        
         annots = []
-        for obj in file_content['truth'][image_index]:
+        for obj in objs:
             annot = [obj[BBOX_CENTER_X], obj[BBOX_CENTER_Y], obj[BBOX_WIDTH], obj[BBOX_HEIGHT], np.argmax(obj[5:])]
             annots.append(annot)
         return np.array(annots)
 
     def load_image(self, i):
-        file_index = int(i / self.evts_per_file)
-        image_index = i % self.evts_per_file
-        file_content = np.load(self.filelist[file_index])
-        return file_content['raw'][image_index]
+        if self.sparse:
+            file_content = np.load(self.filelist[i])
+            return self.importSparse2DenseTensor(file_content[0], (self.config['IMAGE_C'],self.config['IMAGE_H'],self.config['IMAGE_W']))
+        else:
+            file_index = int(i / self.evts_per_file)
+            image_index = i % self.evts_per_file
+            file_content = np.load(self.filelist[file_index])
+            return file_content['raw'][image_index]
 
     # return a batch of images starting at the given index
     def __getitem__(self, idx):